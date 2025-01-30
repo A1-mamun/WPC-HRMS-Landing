1271055e0e4393a92fa16fa95870958f5cd15f0b
A1-mamun <1154almamun@gmail.com>
2025-01-30 19:56:07 +0600

    Add Tailwind CSS configuration and update dependencies; refactor layout and styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,35 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xxs: "400px",
+
+      xs: "480px",
+
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+
+      "3xl": "1700px",
+
+      "4xl": "1880px",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#3F9EB7",
+      },
+      fontFamily: {
+        jura: ["Jura", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
+  darkMode: "class",
   plugins: [],
 } satisfies Config;
